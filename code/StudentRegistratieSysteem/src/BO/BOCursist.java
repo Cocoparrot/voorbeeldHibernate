c22729fc48bfe5c77470194126bf84f92b4e8410
Cocoparrot <samhendrickx97@gmail.com>
2014-12-03 14:19:14 +0100

    Bestanden toegevoegd
    usb webserver, code, documentatie en analyse toegevoegd

---
+++ code/StudentRegistratieSysteem/src/BO/BOCursist.java
@@ -0,0 +1,15 @@
+package BO;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 11627
+ */
+public class BOCursist {
+    
+}
